b0d3a81750975f309bcb9387e94c22e0d8eaedd7
kerim2022 <kerimsha@gmail.com>
2022-09-15 10:46:04 -0700

    task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,7 +10,7 @@
 */
 
 void print_remaining_days(int month, int day, int year)
-{
+	{
 	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
 	if (month >= 3 && day >= 60)
@@ -33,5 +33,5 @@ void print_remaining_days(int month, int day, int year)
 	    printf("Remaining days: %d\n", 365 - day);
 	}
 	}
-}
+	}
 
