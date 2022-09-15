bc397e367916134405872164198d921bafce3e89
kerim2022 <kerimsha@gmail.com>
2022-09-15 06:51:25 -0700

    commenting on the infinite loo

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
 
-
 /**
 * main - causes an infinite loop
 * Return: 0
 */
 
-
-
 int main(void)
-
 {
-
 	int i;
-
-
 
 	printf("Infinite loop incoming :(\n");
 
-
-
 	i = 0;
-
-
 
 	/**
 	* while (i < 10)
@@ -34,11 +23,7 @@ int main(void)
 	* }
 	*/
 
-
 	printf("Infinite loop avoided! \\o/\n");
 
-
-
 	return (0);
-
 }
