ebf9a4bd140954a2eb2406eed947eaacb3218017
kerim2022 <kerimsha@gmail.com>
2022-09-20 11:31:24 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,45 +8,28 @@
  */
 
 int _atoi(char *s)
-
 {
-int nb;
+	int sign;
+	unsigned int num;
+	char *temp;
 
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= '-1';
+		temp++;
+ }
+		 if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++; 
+		} while (*temp >= '0' && *temp <= '9');
+}
 
-
-    nb = _atoi("98");
-
-    printf("%d\n", nb);
-
-    nb = _atoi("-402");
-
-    printf("%d\n", nb);
-
-    nb = _atoi("          ------++++++-----+++++--98");
-
-    printf("%d\n", nb);
-
-    nb = _atoi("214748364");
-
-    printf("%d\n", nb);
-
-    nb = _atoi("0");
-
-    printf("%d\n", nb);
-
-    nb = _atoi("Suite 402");
-
-    printf("%d\n", nb);
-
-    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-
-    printf("%d\n", nb);
-
-    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-
-    printf("%d\n", nb);
-
-
-	return (0);
+	return (num *sign);
 
 }
