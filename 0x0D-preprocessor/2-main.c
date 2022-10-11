0aff5a33f3855ca2147223d45c56c1f3e0dc65b8
kerim2022 <kerimsha@gmail.com>
2022-10-11 01:11:02 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
  * main - prints the name of the file it was compiled from
@@ -10,4 +10,4 @@ int main(void)
 {
 	printf("%s\n", __BASE_FILE__);
 	return (0);
-}
+I}
